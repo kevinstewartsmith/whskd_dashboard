53a2ef3deeeab78f605de43cf25c3e7e34bb9038
Kevin Smith <kevinstewartsmith@gmail.com>
2022-02-18 02:01:06 +0700

    Second Commit -app.js Add app.js file.  -move contents of index.html to headmaster-dashboard.html -move css and images to public file

---
+++ app.js
@@ -0,0 +1,19 @@
+//Requires node.js and express
+const express = require("express");
+
+//Makes an express app
+const app = express();
+app.use(express.static("public"));
+
+//What happens when the user goes to the homepage
+app.get("/", function(req, res) {
+   res.sendFile(__dirname + "/headmaster-dashboard.html");
+})
+
+
+
+//Listening to port 3000 locally. Has a callback function to let the user know whent he server is running
+//The console.log message will show up in the terminal
+app.listen(4000, function() {
+   console.log("Server is running on port 3000");
+})
