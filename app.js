3b208867007085f08b55343ca1c19edb636779d0
Kevin Smith <kevinstewartsmith@gmail.com>
2022-02-18 03:14:10 +0700

    Third Commit: -Change account icon to green -change mdi icons - Copy headmaster dashboard code to teacher dashboard

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.static("public"));
 //What happens when the user goes to the homepage
 app.get("/", function(req, res) {
    res.sendFile(__dirname + "/headmaster-dashboard.html");
+   // res.sendFile(__dirname + "/memory-game.html");
+
 })
 
 
@@ -15,5 +17,5 @@ app.get("/", function(req, res) {
 //Listening to port 3000 locally. Has a callback function to let the user know whent he server is running
 //The console.log message will show up in the terminal
 app.listen(4000, function() {
-   console.log("Server is running on port 3000");
+   console.log("Server is running on port 4000");
 })
