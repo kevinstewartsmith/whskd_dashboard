65fe6ef3c8d080d46a78666ed7919245f5a431d6
Kevin Smith <kevinstewartsmith@gmail.com>
2022-03-12 18:06:52 +0700

    Nest download report functions into another function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -356,45 +356,48 @@ function omg() {
 //     }
 //   });
 // });
+function reportDownload() {
+  var hrClassID = "#homeroomReportFinal";
+  var hrDownloadButtonID = "homeroomDownload"
+  var filename = 'homeroom-report.png'
 
 
+  setUpDownloadPageAsImage(hrDownloadButtonID);
 
-
-setUpDownloadPageAsImage("homeroomDownload");
-
-function setUpDownloadPageAsImage(id) {
-  document.getElementById(id).addEventListener("click", function() {
-    html2canvas($("#homeroomReportFinal")[0]).then(function(canvas) {
-      console.log(canvas);
-      simulateDownloadImageClick(canvas.toDataURL(), 'file-name.png');
+  function setUpDownloadPageAsImage(id) {
+    document.getElementById(id).addEventListener("click", function() {
+      html2canvas($(hrClassID)[0]).then(function(canvas) {
+        console.log(canvas);
+        simulateDownloadImageClick(canvas.toDataURL(), filename);
+      });
     });
-  });
-}
+  }
 
-function simulateDownloadImageClick(uri, filename) {
-  var link = document.createElement('a');
-  if (typeof link.download !== 'string') {
-    window.open(uri);
-  } else {
-    link.href = uri;
-    link.download = filename;
-    accountForFirefox(clickLink, link);
+  function simulateDownloadImageClick(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download !== 'string') {
+      window.open(uri);
+    } else {
+      link.href = uri;
+      link.download = filename;
+      accountForFirefox(clickLink, link);
+    }
+  }
+
+  function clickLink(link) {
+    link.click();
+  }
+
+  function accountForFirefox(click) { // wrapper function
+    let link = arguments[1];
+    document.body.appendChild(link);
+    click(link);
+    document.body.removeChild(link);
   }
 }
-
-function clickLink(link) {
-  link.click();
-}
-
-function accountForFirefox(click) { // wrapper function
-  let link = arguments[1];
-  document.body.appendChild(link);
-  click(link);
-  document.body.removeChild(link);
-}
-
+reportDownload();
 //var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+//var myInput = document.getElementById('myInput')
 
 // myModal.addEventListener('shown.bs.modal', function () {
 //   myInput.focus()
